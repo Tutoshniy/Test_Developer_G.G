a8af60ecf772d71c2f9b2c6ca2012144dafc12c4
Gleb <gleb.gadbrat@mail.ru>
2022-05-07 10:10:53 +0300

    Написана программа

--- CreateMassive/Program.cs
+++ CreateMassive/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string [] Mass = {"a", "b", "c", "d", "e", "f"};
+string [] finMass = new string [new Random().Next(0, 4)];
+int i = new Random().Next(0, Mass.Length - 3);
+Console.WriteLine("Получившийся массив:");
+for (int j = 0; j < finMass.Length; j++)
+{
+    finMass[j] = Mass [i];
+    i++;
+    Console.Write($"{finMass[j]} ");
+}
